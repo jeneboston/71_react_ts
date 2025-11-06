15710ffcfd40de38d9fa25cf44ff5b2c8bce0ca2
jeneboston <smilemusik2013@gmail.com>
2025-11-06 21:16:47 +0100

    lesson07

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import './App.css'
-import Lesson06 from './lessons/Lesson06/Lesson06'
+// Импорты лекций
+// import Lesson06 from "./lessons/Lesson06/Lesson06";
+// import Lesson07 from "lessons/Lesson07/Lesson07";
+
+// Импорты домашних работ
 
 function App() {
-
-
   return (
     <>
-      <Lesson06 />
+      {/* Лекция 6 - TypeScript */}
+      {/* <Lesson06 /> */}
+
+      {/* Лекция 7 - TypeScript: object types */}
+      {/* <Lesson07 /> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
