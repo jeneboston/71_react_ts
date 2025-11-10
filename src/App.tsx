88f9bd5d6268191d6964c7059ba78b689c1aee21
jeneboston <smilemusik2013@gmail.com>
2025-11-10 21:53:38 +0100

    hw09

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import Lesson09 from "lessons/Lesson09/Lesson09";
+// import Lesson09 from "lessons/Lesson09/Lesson09";
 import GlobalStyles from "styles/GlobalStyles";
+import Homework09 from "homeworks/Homework09/Homework09"
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      <Lesson09 />
+      <Homework09 />
+      {/* <Lesson09 /> */}
     </>
   );
 }
