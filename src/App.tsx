92249ed6099cf370a232d2f5bd609f97a45b40e8
jeneboston <smilemusik2013@gmail.com>
2025-11-12 20:46:50 +0100

    hw10

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import Lesson10 from "lessons/Lesson10/Lesson10";
+// import Lesson10 from "lessons/Lesson10/Lesson10";
 // import GlobalStyles from "styles/GlobalStyles";
-// import Homework09 from "homeworks/Homework09/Homework09"
+import Homework10 from "homeworks/Homework10/Homework10"
 
 function App() {
   return (
     <>
       {/* <GlobalStyles /> */}
-      {/* <Homework10 /> */}
-      <Lesson10 />
+      <Homework10 />
+      {/* <Lesson10 /> */}
     </>
   );
 }
---
+++ src/homeworks/Homework10/styles.ts
@@ -0,0 +1,19 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  display: grid;
+  gap: 16px;
+  padding: 24px;
+`;
+
+export const Inputs = styled.div`
+  display: grid;
+  gap: 12px;
+  max-width: 460px;
+`;
+
+export const Result = styled.div`
+  font-size: 20px;
+  line-height: 1.4;
+  min-height: 28px;
+`;
